01e29090e659db146a0764a0556b21444d339918
MohammadTalha <m.talhaleo2002@gmail.com>
2023-04-03 13:48:53 -0700

    Basic Syntax of Cypress

---
+++ cypress/e2e/mytest.cy.js
@@ -0,0 +1,38 @@
+
+
+
+
+describe('My First Test', () => {
+  it('test1', () => {
+
+      // Steps
+
+   
+  })
+
+
+//  Suppose you have to write another test same thing you can just copy here and write another entry 
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// When you come to browser there are two types of specs
+  // 1: Scaffold example specs (When you create specs , sample examples files should you see in E2E testing)
+
+
+  //  2: Create new empty spec (When you want to ceate a spec, and you can give any name to the file with extension spec cy.js)   Or Either you can create empty specs directly under your E2E folder.
+
+// Extension Spec File ('fileName.cy.js')
+      //  .cy.js is extension
