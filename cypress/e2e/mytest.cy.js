87a30881705398c99d1f49bd5134f5a43f312af9
MohammadTalha <m.talhaleo2002@gmail.com>
2023-04-05 01:10:57 -0700

    Test Cases Runs

--- cypress/e2e/mytest.cy.js
+++ cypress/e2e/mytest.cy.js
@@ -3,17 +3,55 @@
 
 
 describe('My First Test', () => {
-  it('test1', () => {
+  it('verify title-positive', () => {
 
-      // Steps
 
-   
+    // So this 'cy' is the root model of Cypress,by using 'cy' we be able to access all the commands from Cypress 
+
+
+    // So 'title' is method ,in this 'title', 'title' will be written and once you return the 'title' that 'title' should be equal to something else we need to compare that.
+    // So for that we need to call one more method called 'should'.
+    // And here we need to specify one keyword called equal 'eq'( as a parameter).
+    // And here in the second parameter, we can specify the title of the current web page 
+
+    // You can use single/double quotations in parameters and others ,no difference in JavaScript
+
+    // cy.title().should('eq','OrangeHRM')
+    // This is one Assertin
+
+
+      cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+      cy.title().should('eq','OrangeHRM')
+
   })
+
+
+  // Here one test should be passed and one test should be failed
+
+  it('verify title-negative', () => {     
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+      cy.title().should('eq','OrangeHRM123')
+  })
+
+
+  // And when you open cypress in browser, you see specs on browser mytest.cy.js .You can click it once ,it will start running your test. And You can check that one test should be passed and second one got failed
+
+
+
+
+
+
+
+
+
 
 
 //  Suppose you have to write another test same thing you can just copy here and write another entry 
 
 })
+
+
+// So One Spec file have nth number of suite number of describe sections or we can say describe block can have multiple it blocks and every it block is representing one test
 
 
 
